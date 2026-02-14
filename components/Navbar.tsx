2c44fe6c2e4e1cf277306671864f7d41dbf16f3e
Joshua Kibwage <joshuakibwage23@gmail.com>
2026-02-14 11:50:10 +0300

    feat: configure light and dark mode theme

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,91 @@
-import React from 'react'
+"use client"
+
+import React from 'react';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+//icons
+import { Moon, LogOut, Settings, User, Sun } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
 
 const Navbar = () => {
+
+    const { setTheme } = useTheme()
+
   return (
-    <div>
-      navbar
-    </div>
+    <nav className="p-4 flex justify-between items-center w-[94%] mx-auto">
+      {/* left */}
+      collapse button
+      {/* right */}
+        <div className="flex items-center gap-4">
+            <Link href="/">Dashboard</Link>
+            
+            {/* Theme Menu */}
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            {/* User Menu */}
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>JK</AvatarFallback>
+                        </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent sideOffset={4}>
+                    <DropdownMenuGroup>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuItem>
+                            <User />
+                            Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings />
+                            Settings
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem variant="destructive">
+                            <LogOut />
+                            Sign out
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    </nav>
   )
 }
 
